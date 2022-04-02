2b570126dec487975e5e2628146f5484c95ed9c4
SumantKumar <sumant.kumar@antino.io>
2022-04-02 15:03:56 +0530

    randomize array

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,33 +1,40 @@
 import React from 'react'
-import { useState } from 'react';
-import {Arrayy} from './index'
+import { useState, useEffect } from 'react';
+import { Arrayy } from './index'
 export default function Main() {
-  const [size,setSize]=useState(25);
-  const [speed,setspeed]=useState(1);
-  const [click,setClick]=useState(false);
-  const [change,setChange]=useState(false);
+  const [size, setSize] = useState(35);
+  const [speed, setspeed] = useState(1);
 
- 
+  useEffect(() => {
+    console.log('render', size);
+  }, [size])
+
+
   return (
     <div className='flex justify-center items-center w-[100vw] h-[93vh]'>
       <div className='flex justify-center items-end relative glass-section h-[85%] w-4/5'>
         <div className='w-full absolute top-5 '>
-          <div className='text-lg text-gray-700 mx-auto w-4/5   flex text-center justify-between'>
+          <div className='text-lg text-gray-700 mx-auto w-3/4   flex text-center justify-between'>
             <div className='flex flex-col'>
               <label>Size of Array</label>
-              <input className='w-[200px]' type="range" min="19" max="51" onChange={(e) => {setSize(e.target.value);setChange(true); console.log(size)} } />
+              <input className='w-[200px]' type="range" min="35" max="65" onChange={(e) => { setSize(e.target.value) }} />
             </div>
             <div className='flex flex-col'>
-              <label>Speed of Array</label>
-              <input className='w-[200px] ' type="range" min="1" max="5" onChange={(e) => {setspeed(e.target.value);setChange(true); console.log(speed)}}/></div>
-            <button className='p-[10px] btn' onClick={()=>{setClick(true);setChange(false); console.log(click)}}>Generate New Array</button>
+              <label>Speed of Sort</label>
+              <input className='w-[200px] ' type="range" min="1" max="5" onChange={(e) => { setspeed(e.target.value) }} />
+            </div>
+            <div className='flex justify-center items-center'>
+              <button className='btn py-3 px-6'>Start Sort</button>
+            </div>
           </div>
+
         </div>
         {
-          (click||change)  ?  <Arrayy size={size}  speed={speed}/>:
-          <Arrayy size={25}  speed={1}/>
+          (console.log(size))}
+        {
+          <Arrayy size={size} speed={speed} />
         }
-        </div>
+      </div>
     </div>
   )
 }
